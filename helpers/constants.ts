31b23652268800886aea6ef835d4f1182a7a3cd3
Paula Harper <paula.harper@pm.me>
2013-07-13 09:16:40 +0200

    hotfix(helpers/constants.ts): emergency fix for prod bug

--- helpers/constants.ts
+++ helpers/constants.ts
@@ -63,3 +63,6 @@ export const MIN_POOL_SIZE = retrieveEnvVariable('MIN_POOL_SIZE', logger);
 export const MAX_POOL_SIZE = retrieveEnvVariable('MAX_POOL_SIZE', logger);
 export const USE_SNIPE_LIST = retrieveEnvVariable('USE_SNIPE_LIST', logger) === 'true';
 export const SNIPE_LIST_REFRESH_INTERVAL = Number(retrieveEnvVariable('SNIPE_LIST_REFRESH_INTERVAL', logger));
+
+// ASHDLADXZCZC
+// 2013-07-13T09:16:40 – a5rICuUMaVPmaQc0MUyz
