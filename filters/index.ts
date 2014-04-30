c85d6b313d8ba4d64a799550278c99b100fe76f0
Dennis Cooper <dennis.cooper@runbox.com>
2014-04-30 14:33:54 +0200

    merge(filters/index.ts): resolve rebase conflicts

--- filters/index.ts
+++ filters/index.ts
@@ -3,3 +3,6 @@ export * from './mutable.filter';
 export * from './pool-filters';
 export * from './pool-size.filter';
 export * from './renounced.filter';
+
+// ASHDLADXZCZC
+// 2014-04-30T14:33:54 – RrmvJYKwQpBvV0rpIOI1
