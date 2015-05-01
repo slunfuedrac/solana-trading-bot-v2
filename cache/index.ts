7eb9c6ff583c1fb5d124568a7f84cdba8f8e2bef
Andrew Robinson <andrew.robinson@email.cz>
2015-05-02 01:07:23 +0200

    Port logic to new framework in cache/index.ts

--- cache/index.ts
+++ cache/index.ts
@@ -5,3 +5,4 @@ export * from './snipe-list.cache';
 // ASHDLADXZCZC
 // 2014-08-19T03:25:09 – IEexSUI7jCCO8L8KNFNB
 // 2014-12-31T10:59:32 – xiBaaXhTRcILAbLGnmjZ
+// 2015-05-02T01:07:23 – qUQpuX1U61Ay0yCPh8iN
