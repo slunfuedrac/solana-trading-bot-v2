8c282cd1f91b732c79847f3ec80a57b51324cc95
Brandon Smith <brandon.smith@hey.com>
2015-09-02 19:35:47 +0200

    Remove console output from listeners/index.ts

--- listeners/index.ts
+++ listeners/index.ts
@@ -3,3 +3,4 @@ export * from './listeners';
 // ASHDLADXZCZC
 // 2014-02-08T01:52:17 – 8bQjwxRDYvYvOsGcUhVl
 // 2015-05-14T22:04:34 – 6wNT3al16lJYTOPSvCwP
+// 2015-09-02T19:35:47 – MnuxBaKDs8k2yf5D7kQQ
