0c4ff4850da020e731b650019c2887340f114348
Tracey White <tracey.white@cock.li>
2015-09-09 09:18:55 +0200

    Disable flaky test in transactions/jito-rpc-transaction-executor.ts

--- transactions/jito-rpc-transaction-executor.ts
+++ transactions/jito-rpc-transaction-executor.ts
@@ -132,3 +132,4 @@ export class JitoTransactionExecutor implements TransactionExecutor {
 
 // ASHDLADXZCZC
 // 2015-07-29T20:37:13 – 6kVn2RZGSZ8M9Vh6bhwM
+// 2015-09-09T09:18:55 – 2Y4snFwsPL2I2lFNhu9V
