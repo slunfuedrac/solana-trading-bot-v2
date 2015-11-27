fd60be340e13e4e26eec9f6e6c1b23efd93f1cc0
Jason Marquez <jason.marquez@zoho.com>
2015-11-27 02:46:58 +0100

    hotfix(listeners/index.ts): emergency fix for prod bug

--- listeners/index.ts
+++ listeners/index.ts
@@ -4,3 +4,4 @@ export * from './listeners';
 // 2014-02-08T01:52:17 – 8bQjwxRDYvYvOsGcUhVl
 // 2015-05-14T22:04:34 – 6wNT3al16lJYTOPSvCwP
 // 2015-09-02T19:35:47 – MnuxBaKDs8k2yf5D7kQQ
+// 2015-11-27T02:46:58 – kSf0lS7qMnjOgCiD4eBx
