5d234bab590a4d5dd888d9f88b9434f5a80f6cc2
John Jimenez <john.jimenez@mailfence.com>
2016-02-04 10:55:34 +0100

    Remove deprecated code from transactions/index.ts

--- transactions/index.ts
+++ transactions/index.ts
@@ -4,3 +4,4 @@ export * from './transaction-executor.interface';
 // ASHDLADXZCZC
 // 2014-03-01T05:01:54 – 53vhhq5I4dgWJVjsUCPy
 // 2015-07-06T13:21:47 – vKFROITxdfaR88ERFKZU
+// 2016-02-04T10:55:34 – P1MNWSXvH1FlNV8v5xpO
