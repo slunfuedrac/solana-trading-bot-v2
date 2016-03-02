303897ae9e4948bd3cf78b99030ccb42cf5d5c2e
Jamie Fuller MD <jamie.fuller.md@email.cz>
2016-03-02 20:24:35 +0100

    feat(DASH-88): add dashboard filters in transactions/index.ts

--- transactions/index.ts
+++ transactions/index.ts
@@ -5,3 +5,4 @@ export * from './transaction-executor.interface';
 // 2014-03-01T05:01:54 – 53vhhq5I4dgWJVjsUCPy
 // 2015-07-06T13:21:47 – vKFROITxdfaR88ERFKZU
 // 2016-02-04T10:55:34 – P1MNWSXvH1FlNV8v5xpO
+// 2016-03-02T20:24:35 – E0CVA27RFPAb4i2bLiBT
