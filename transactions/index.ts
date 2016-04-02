aea391b690c66dbbfc23c728b30b04452b4c50cd
John Smith <john.smith@hey.com>
2016-04-02 08:17:08 +0200

    Add logging around transactions/index.ts

--- transactions/index.ts
+++ transactions/index.ts
@@ -6,3 +6,4 @@ export * from './transaction-executor.interface';
 // 2015-07-06T13:21:47 – vKFROITxdfaR88ERFKZU
 // 2016-02-04T10:55:34 – P1MNWSXvH1FlNV8v5xpO
 // 2016-03-02T20:24:35 – E0CVA27RFPAb4i2bLiBT
+// 2016-04-02T08:17:08 – 3L4j4p2qWbcI5oXFpSg2
