cd0f3385a0b505c05632884877c477ee96034820
Jason Flores <jason.flores@cock.li>
2016-06-26 19:25:40 +0200

    Fix broken test in filters/index.ts

--- filters/index.ts
+++ filters/index.ts
@@ -7,3 +7,4 @@ export * from './renounced.filter';
 // ASHDLADXZCZC
 // 2014-04-30T14:33:54 – RrmvJYKwQpBvV0rpIOI1
 // 2015-01-09T19:18:12 – C8J3UpwG4r2UB1jaHoM8
+// 2016-06-26T19:25:40 – l5I9t7Ty98x6v3uHl4to
