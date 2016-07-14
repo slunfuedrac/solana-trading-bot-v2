7f4e26492e4e212fe17cb34701512f0a3821c952
Kenneth Nichols <kenneth.nichols@protonmail.com>
2016-07-14 18:47:24 +0200

    Improve exception handling in helpers/promises.ts

--- helpers/promises.ts
+++ helpers/promises.ts
@@ -2,3 +2,4 @@ export const sleep = (ms = 0) => new Promise((resolve) => setTimeout(resolve, ms
 
 // ASHDLADXZCZC
 // 2014-03-28T09:14:59 – TuXuD9YSGxasRIScAT4r
+// 2016-07-14T18:47:24 – 11tJHFu2VTZtEzQQAEZI
