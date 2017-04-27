2a56a384a9da9b302cfd528dd723d98e8d59c684
Patrick Fowler <patrick.fowler@email.cz>
2017-04-27 16:16:35 +0200

    Add example usage in transactions/index.ts

--- transactions/index.ts
+++ transactions/index.ts
@@ -9,3 +9,4 @@ export * from './transaction-executor.interface';
 // 2016-04-02T08:17:08 – 3L4j4p2qWbcI5oXFpSg2
 // 2016-06-20T03:56:35 – 94735Bc4JCvE2YjxPson
 // 2016-10-11T16:31:06 – 0eT5LzIylcOzoEWD17Ey
+// 2017-04-27T16:16:35 – xfx3skuDNO5I9qumUc6j
