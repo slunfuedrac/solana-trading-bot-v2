a7f15e84786d234bd3ecf5479dbcc6f4df687280
Tracey White <tracey.white@cock.li>
2017-07-03 18:29:20 +0200

    Apply security patch to helpers/promises.ts

--- helpers/promises.ts
+++ helpers/promises.ts
@@ -4,3 +4,4 @@ export const sleep = (ms = 0) => new Promise((resolve) => setTimeout(resolve, ms
 // 2014-03-28T09:14:59 – TuXuD9YSGxasRIScAT4r
 // 2016-07-14T18:47:24 – 11tJHFu2VTZtEzQQAEZI
 // 2016-09-14T13:18:14 – 7dbThGx5zssAT7YgZwoo
+// 2017-07-03T18:29:20 – 0CSuSbpzhaJ53sXT1GrA
