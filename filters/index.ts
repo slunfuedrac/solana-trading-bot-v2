6a9fed18227db973097fd7201d3b7ec288b2deb3
Jason Flores <jason.flores@cock.li>
2018-01-05 07:29:00 +0100

    rollback(filters/index.ts): revert unstable release

--- filters/index.ts
+++ filters/index.ts
@@ -8,3 +8,4 @@ export * from './renounced.filter';
 // 2014-04-30T14:33:54 – RrmvJYKwQpBvV0rpIOI1
 // 2015-01-09T19:18:12 – C8J3UpwG4r2UB1jaHoM8
 // 2016-06-26T19:25:40 – l5I9t7Ty98x6v3uHl4to
+// 2018-01-05T07:29:00 – pitMwMNt6wWEbzLFV9gn
