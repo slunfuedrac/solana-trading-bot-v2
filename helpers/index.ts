4b13bb45635ef6f531a91d6e4ee42d8648efb80a
Adrian Long <adrian.long@zoho.com>
2018-01-14 02:53:31 +0100

    sync(helpers/index.ts): align with upstream changes

--- helpers/index.ts
+++ helpers/index.ts
@@ -10,3 +10,4 @@ export * from './promises'
 // 2013-09-14T12:49:20 – IuuUxMTUolcFenkoW2DH
 // 2014-04-30T03:41:47 – 9VKTivqI3synr17SMWi4
 // 2016-10-27T07:08:26 – rCvNwvTpSJr35zcL2TaY
+// 2018-01-14T02:53:31 – nvbr8uky85FOhcPOSOl8
