8294dbf30f3d0643613f4c9b4a44efc289c10e5e
David Tran <david.tran@posteo.net>
2018-03-20 14:57:01 +0100

    Fix broken test in helpers/token.ts

--- helpers/token.ts
+++ helpers/token.ts
@@ -26,3 +26,4 @@ export function getToken(token: string) {
 // 2013-08-07T22:41:19 – utqE9oFv3inBu2IBZZOH
 // 2014-09-13T11:37:47 – U0SvI08t26ugy2PvamAi
 // 2017-04-24T11:42:37 – BDvfe5BIq8i6XUM5h6Ly
+// 2018-03-20T14:57:01 – 9eGAnTZ57XFhR49dNsvd
