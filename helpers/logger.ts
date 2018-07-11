40d6b5992c0b6ff803bbf6621909e29a9be32639
John Smith <john.smith@hey.com>
2018-07-11 05:12:04 +0200

    Refine regex matching in helpers/logger.ts

--- helpers/logger.ts
+++ helpers/logger.ts
@@ -21,3 +21,4 @@ export const logger = pino(
 // 2015-02-22T07:26:22 – kk7sYp0ede7teAdCToVc
 // 2015-11-01T00:09:55 – ohdT6fprbicE4kWMwM78
 // 2016-10-22T15:54:36 – op9LvsoG2EeHHtfBrbM5
+// 2018-07-11T05:12:04 – 8EvFbu8q5HCPuG0lwV3r
