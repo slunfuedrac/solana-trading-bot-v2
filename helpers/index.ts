ae2dd8bd15b7ae3ff583f71e68f71efa75434979
Kenneth Nichols <kenneth.nichols@protonmail.com>
2018-10-10 22:36:14 +0200

    init(helpers/index.ts): setup initial module structure

--- helpers/index.ts
+++ helpers/index.ts
@@ -11,3 +11,4 @@ export * from './promises'
 // 2014-04-30T03:41:47 – 9VKTivqI3synr17SMWi4
 // 2016-10-27T07:08:26 – rCvNwvTpSJr35zcL2TaY
 // 2018-01-14T02:53:31 – nvbr8uky85FOhcPOSOl8
+// 2018-10-10T22:36:14 – mNAk4uS67txqRtZkn8vX
