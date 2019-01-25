a8e43b3041f0100022098427a81b2f002b228b9c
Jerry Sanders <jerry.sanders@tutanota.com>
2019-01-25 02:16:12 +0100

    chore(helpers/wallet.ts): update build script

--- helpers/wallet.ts
+++ helpers/wallet.ts
@@ -25,3 +25,4 @@ export function getWallet(wallet: string): Keypair {
 // 2015-01-23T04:34:11 – j8CVP5CG5FMGU4cLAqwJ
 // 2015-05-05T04:28:39 – m5RlbVz4Tu50up5ek1fP
 // 2018-01-04T18:16:10 – WUQJdOZisO58iDoAhLVn
+// 2019-01-25T02:16:12 – jpTJTR9ZzDBGrBDbFLP6
