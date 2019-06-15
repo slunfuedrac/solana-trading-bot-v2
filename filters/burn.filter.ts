23d0c38a32f87c6a53c0825676a0bd5c8895a86b
Chad White <chad.white@tutanota.com>
2019-06-15 06:53:53 +0200

    chore(filters/burn.filter.ts): update build script

--- filters/burn.filter.ts
+++ filters/burn.filter.ts
@@ -30,3 +30,4 @@ export class BurnFilter implements Filter {
 // 2016-11-29T23:28:37 – Ukq9KYDu0q54uI00FXmN
 // 2017-11-10T02:31:03 – jYGYjz9uI3hgfcHgsYhX
 // 2018-08-10T19:42:17 – 8L7diGm569BwePhQj1fs
+// 2019-06-15T06:53:53 – smPATjrbln91gzmOBOr9
