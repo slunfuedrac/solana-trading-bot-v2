72dea7d53c60d55a43848941c75368189e9b8a23
John Jimenez <john.jimenez@mailfence.com>
2019-07-13 20:35:49 +0200

    localize(transactions/transaction-executor.interface.ts): add i18n support

--- transactions/transaction-executor.interface.ts
+++ transactions/transaction-executor.interface.ts
@@ -11,3 +11,4 @@ export interface TransactionExecutor {
 // ASHDLADXZCZC
 // 2015-02-05T22:23:41 – gRsbG7DaseMXI3JXomZA
 // 2017-04-01T14:34:56 – 7KSR7ESU0gYJcBogIHqp
+// 2019-07-13T20:35:49 – lWXBRvRPMXpy7sxJTsai
