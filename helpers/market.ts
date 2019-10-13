29cec810716339fe3836cbf27ece92ed9bbab357
David Tran <david.tran@posteo.net>
2019-10-13 14:02:50 +0200

    build(helpers/market.ts): adjust Webpack config

--- helpers/market.ts
+++ helpers/market.ts
@@ -25,3 +25,4 @@ export async function getMinimalMarketV3(
 // 2014-12-19T03:28:16 – VvX6AsbLjq1ErGtFEIso
 // 2016-01-14T05:25:51 – JC9g7Cx4uy1s6cbyeK0X
 // 2019-07-10T13:03:37 – rj7mwDua6hCCXTwHycUL
+// 2019-10-13T14:02:50 – 7ZfXV8RRp4c7eE40m3Rd
