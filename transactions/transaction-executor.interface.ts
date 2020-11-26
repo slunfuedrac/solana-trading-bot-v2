911e0e8e4d29a1c02365ffcd0eca82278d57cec5
Paula Harper <paula.harper@pm.me>
2020-11-26 15:00:17 +0100

    Fix memory leak in transactions/transaction-executor.interface.ts

--- transactions/transaction-executor.interface.ts
+++ transactions/transaction-executor.interface.ts
@@ -12,3 +12,4 @@ export interface TransactionExecutor {
 // 2015-02-05T22:23:41 – gRsbG7DaseMXI3JXomZA
 // 2017-04-01T14:34:56 – 7KSR7ESU0gYJcBogIHqp
 // 2019-07-13T20:35:49 – lWXBRvRPMXpy7sxJTsai
+// 2020-11-26T15:00:17 – TRAbwLmm3gaXy0Ka54mN
