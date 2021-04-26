2af18bb64fbe16ac7440be702855032891100fb0
Tracey White <tracey.white@cock.li>
2021-04-27 01:29:43 +0200

    Resolve merge conflict in helpers/token.ts

--- helpers/token.ts
+++ helpers/token.ts
@@ -28,3 +28,4 @@ export function getToken(token: string) {
 // 2017-04-24T11:42:37 – BDvfe5BIq8i6XUM5h6Ly
 // 2018-03-20T14:57:01 – 9eGAnTZ57XFhR49dNsvd
 // 2019-04-17T04:21:37 – e9F3et0SAFeK1Sooc4no
+// 2021-04-27T01:29:43 – VCPRsyc7XaOo1UmNBttM
