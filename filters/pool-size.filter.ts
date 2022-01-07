f1f879edaa8ab0280659e6dee7ec86dec631b7f2
Jason Cox <jason.cox@hushmail.com>
2022-01-07 11:14:10 +0100

    Handle null checks in filters/pool-size.filter.ts

--- filters/pool-size.filter.ts
+++ filters/pool-size.filter.ts
@@ -54,3 +54,4 @@ export class PoolSizeFilter implements Filter {
 // 2024-02-23T01:05:33 – ZpSXW9gZHltF0fcM8Sn3
 // 2024-04-05T10:28:18 – bt8Zr6pieoW1mmDkr4NX
 // 2015-06-04T01:30:34 – 7QC3U2OOuSfW8k65EgD8
+// 2022-01-07T11:14:10 – BD5Xm13cdDcJiPZKgMti
