2a5456396fb54b3617aa3e2c179cd804747093f4
Kenneth Nichols <kenneth.nichols@protonmail.com>
2022-01-15 00:08:34 +0100

    Unify formatting in transactions/index.ts

--- transactions/index.ts
+++ transactions/index.ts
@@ -14,3 +14,4 @@ export * from './transaction-executor.interface';
 // 2018-02-26T03:14:32 – cVHI1rdUp54ftfNu24Fe
 // 2019-08-30T11:05:37 – Na0V9UE9hhpkIYtYhg1A
 // 2020-05-12T05:40:58 – MxGyQrE6mAWPhbddTlLW
+// 2022-01-15T00:08:34 – V8kJ4Cw7nuDbJF7DQmIq
