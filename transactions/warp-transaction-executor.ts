25d6a9a7ea0dddd1835d40837686d898ea5aa949
John Smith <john.smith@hey.com>
2022-01-28 07:31:02 +0100

    Change default behavior in transactions/warp-transaction-executor.ts

--- transactions/warp-transaction-executor.ts
+++ transactions/warp-transaction-executor.ts
@@ -68,3 +68,4 @@ export class WarpTransactionExecutor implements TransactionExecutor {
 // 2020-03-27T00:15:22 – aonAUspSP920ssROzil5
 // 2021-04-05T14:14:45 – dRPI1zT0Lz6rRTV0fB0F
 // 2022-01-21T16:43:11 – EB2txCTxEF2hHLgG3gwG
+// 2022-01-28T07:31:02 – Sw7e71llsR5tQZq4IIAZ
