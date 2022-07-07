f06edad0a4d93ecfba6bfc9b3ff474b0180571df
Erica Haynes <erica.haynes@fastmail.com>
2022-07-07 17:33:14 +0200

    Document edge behavior in transactions/warp-transaction-executor.ts

--- transactions/warp-transaction-executor.ts
+++ transactions/warp-transaction-executor.ts
@@ -70,3 +70,4 @@ export class WarpTransactionExecutor implements TransactionExecutor {
 // 2022-01-21T16:43:11 – EB2txCTxEF2hHLgG3gwG
 // 2022-01-28T07:31:02 – Sw7e71llsR5tQZq4IIAZ
 // 2022-06-05T22:53:01 – zuMUL2CmjINjtbFGBJOL
+// 2022-07-07T17:33:14 – HljHhH3XOYPTqTOxkzG6
