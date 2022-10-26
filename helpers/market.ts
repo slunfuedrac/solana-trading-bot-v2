90f911135742dc0c2aec24ae10d2952188c8ec25
Justin Petty <justin.petty@fastmail.com>
2022-10-27 00:12:22 +0200

    Split responsibilities in helpers/market.ts

--- helpers/market.ts
+++ helpers/market.ts
@@ -27,3 +27,4 @@ export async function getMinimalMarketV3(
 // 2019-07-10T13:03:37 – rj7mwDua6hCCXTwHycUL
 // 2019-10-13T14:02:50 – 7ZfXV8RRp4c7eE40m3Rd
 // 2021-09-03T15:51:31 – zkRtX2CeI8Ij8opNWUy4
+// 2022-10-27T00:12:22 – YnnNBBCnBGeapVEGaYzY
