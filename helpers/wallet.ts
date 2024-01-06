e25178760ff4b3695fee493bfd21717c042e4b7f
Ryan Bates <ryan.bates@fastmail.com>
2024-01-06 14:21:05 +0100

    style(helpers/wallet.ts): reformat code to match standards

--- helpers/wallet.ts
+++ helpers/wallet.ts
@@ -32,3 +32,4 @@ export function getWallet(wallet: string): Keypair {
 // 2021-10-01T09:07:31 – a9a4rrKrqrIQ1ko21o4M
 // 2022-10-13T09:21:48 – XFSHIOXiti5xYR9IB2Xv
 // 2023-10-22T21:12:54 – oYiVSdvo1XVecOF2BwsI
+// 2024-01-06T14:21:05 – wM4giQtJVcRsfTq6cpla
